12bdf8cdbc0bea15aad1922104aea74469b3c9d1
adrian-amigues <adrian.amigues@gmail.com>
2026-01-30 17:37:03 +0100

    setup basic cylindric mask placer manager

---
+++ Assets/Scripts/MaskPlacementManager.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class MaskPlacementManager : MonoBehaviour {
+  public void SetCylindricalPosition(float angleDeg, float radius, float height) {
+    float angleRad = angleDeg * Mathf.Deg2Rad;
+
+    Vector3 localPosition = new Vector3(
+      radius * Mathf.Cos(angleRad),
+      height,
+      radius * Mathf.Sin(angleRad)
+    );
+    transform.position = transform.position + localPosition;
+    transform.LookAt(transform);
+  }
+}
