57c3201573acfe76e2719c5b911cf7fa942044d8
adrian-amigues <adrian.amigues@gmail.com>
2026-01-31 00:37:52 +0100

    add raycast with spotlight activation

---
+++ Assets/Scripts/MaskSelectable.cs
@@ -0,0 +1,28 @@
+﻿using PrimeTween;
+using UnityEngine;
+
+public class MaskSelectable : MonoBehaviour {
+  [SerializeField] private Light selectionLight;
+  [SerializeField] private float fadeDuration = 3f;
+
+  private float maxIntensity = 0f;
+  private Tween currentTween;
+
+  private void Awake() {
+    maxIntensity = selectionLight.intensity;
+  }
+
+  private void Start() {
+    selectionLight.intensity = 0f;
+  }
+
+  private void OnDestroy() {
+    currentTween.Stop();
+  }
+
+  public void SetSelected(bool selected) {
+    currentTween.Stop();
+    float targetIntensity = selected ? maxIntensity : 0f;
+    currentTween = Tween.LightIntensity(selectionLight, targetIntensity, fadeDuration, Ease.OutQuad);
+  }
+}
---
+++ Assets/Scripts/MaskSelector.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class MaskSelector : MonoBehaviour {
+  [SerializeField] private float maxDistance = 20f;
+  [SerializeField] private LayerMask maskLayer;
+
+  private MaskSelectable currentSelection;
+
+  private void Update() {
+    Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+    MaskSelectable newSelection = null;
+
+    if (Physics.Raycast(ray, out RaycastHit hit, maxDistance, maskLayer)) {
+      newSelection = hit.collider.GetComponent<MaskSelectable>();
+    }
+
+    if (newSelection != currentSelection) {
+      currentSelection?.SetSelected(false);
+      newSelection?.SetSelected(true);
+      currentSelection = newSelection;
+    }
+  }
+
+  private void OnDrawGizmos() {
+    Gizmos.color = Color.green;
+    Ray ray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0));
+    Gizmos.DrawRay(ray.origin, ray.direction * maxDistance);
+  }
+}
