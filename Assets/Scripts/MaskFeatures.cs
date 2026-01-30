0ab1b99406b819a9f1f7a21c7017bc3ef824799a
adrian-amigues <adrian.amigues@gmail.com>
2026-01-30 16:59:16 +0100

    setup basic mask feature definition logic

--- Assets/Scripts/MaskFeatures.cs
+++ Assets/Scripts/MaskFeatures.cs
@@ -1,16 +1,41 @@
+using System.Collections.Generic;
 using UnityEngine;
 
 public class MaskFeatures : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
+    [Header("This mask's current features")]
+    [SerializeField, Range(0, 2)] private int maneFeature;
+    [SerializeField, Range(0, 2)] private int hornFeature;
+    [SerializeField, Range(0, 2)] private int expressionFeature;
 
+    [Header("All mask feature elements")]
+    [SerializeField] private List<GameObject> maneElements;
+    [SerializeField] private List<GameObject> hornElements;
+    [SerializeField] private List<GameObject> expressionElements;
+
+    private void Start()
+    {
+        UpdateMaskVisuals();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnValidate()
     {
+        UpdateMaskVisuals();
+    }
 
+    private void UpdateMaskVisuals()
+    {
+        for (int i = 0; i < maneElements.Count; i++)
+        {
+            maneElements[i].SetActive(i == maneFeature);
+        }
+        for (int i = 0; i < hornElements.Count; i++)
+        {
+            hornElements[i].SetActive(i == hornFeature);
+        }
+        for (int i = 0; i < expressionElements.Count; i++)
+        {
+            expressionElements[i].SetActive(i == expressionFeature);
+        }
     }
 }
