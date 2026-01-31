cabb6534399abaa199037139b14166244c89e51d
Paul Niezborala <pauniezbo@gmail.com>
2026-01-31 13:54:47 +0100

    Introduce hard-coded levels

---
+++ Assets/Scripts/ILevel.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public interface ILevel {
+  public Dictionary<MaskFeatures.Configuration, CylindricalVector3> GetMasks();
+  public bool IsIntruder(MaskFeatures.Configuration mask);
+}
+
+public class AbstractLevel : ScriptableObject, ILevel {
+  public virtual Dictionary<MaskFeatures.Configuration, CylindricalVector3> GetMasks() => throw new System.NotImplementedException();
+  public virtual bool IsIntruder(MaskFeatures.Configuration mask) => throw new System.NotImplementedException();
+}
