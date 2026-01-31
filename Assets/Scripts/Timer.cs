ad311ec8feee5e8bd22537f8df95ba6966fec149
adrian-amigues <adrian.amigues@gmail.com>
2026-01-31 19:25:25 +0100

    add timer UI

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,14 +1,40 @@
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-public class Timer : MonoBehaviour
-{
-  [SerializeField] private int seconds;
+public class Timer : MonoBehaviour {
+  [SerializeField] private float timeLimitSeconds = 30f;
+
+  [SerializeField] private Image uiFillBar;
 
   private float startTime = -1.0f;
   public bool Started => startTime >= 0;
 
+  public float RemainingTimeNormalized() {
+    if (Started) {
+      var elapsed = Time.time - startTime;
+      return Mathf.Clamp01(elapsed / timeLimitSeconds);
+    } else {
+      return 0f;
+    }
+  }
+
   public delegate void TimerEvent();
   public event TimerEvent OnTimerElapsed = delegate { };
+
+  void Start() {
+    StartCountdown();
+  }
+
+  private void Update() {
+    if (!Started) return;
+
+    UpdateUI();
+
+    if (Time.time - startTime > timeLimitSeconds) {
+      OnTimerElapsed?.Invoke();
+      StopCountdown();
+    }
+  }
 
   public void StartCountdown() {
     startTime = Time.time;
@@ -22,10 +48,9 @@ public class Timer : MonoBehaviour
     StartCountdown();
   }
 
-  private void Update() {
-    if (Started && Time.time - startTime > seconds) {
-      OnTimerElapsed?.Invoke();
-      StopCountdown();
+  private void UpdateUI() {
+    if (uiFillBar != null) {
+      uiFillBar.fillAmount = 1f - RemainingTimeNormalized();
     }
   }
 }
